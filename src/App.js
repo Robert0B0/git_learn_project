cfe58bae9d08f00504ae2cd2d58496396df9cc24
Robert <robert.balog000@gmail.com>
2022-02-09 20:55:36 +0200

    modified branch

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>Master Branch</h2>
-      <p>Original Content</p>
+      <h2>First Branch</h2>
+      <p>Modified Content</p>
     </div>
   );
 }
