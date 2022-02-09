96d7f4190cb70782a6adc5a35cfb1c4156c75e81
Robert <robert.balog000@gmail.com>
2022-02-09 20:53:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Master Branch</h2>
+      <p>Original Content</p>
+    </div>
+  );
+}
+
+export default App;
