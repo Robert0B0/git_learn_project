31231da63413cc4e779621bb6550ecbd249799cc
Robert <robert.balog000@gmail.com>
2022-02-09 21:00:02 +0200

    heavily modified branch child

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>First Branch</h2>
-      <p>Modified Content</p>
+      <h2>First Child Branch</h2>
+      <p>Heavily Modified Content</p>
     </div>
   );
 }
